86a5b9d56ee9f0251aeaf54068332897dce7f620
BuildTools <randysim9@gmail.com>
2021-05-15 17:31:10 -0400

    render event done

---
+++ src/main/java/GandyClient/mixins/client/renderer/EntityRendererMixin.java
@@ -0,0 +1,19 @@
+package GandyClient.mixins.client.renderer;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import GandyClient.events.impl.RenderEvent;
+import net.minecraft.client.renderer.EntityRenderer;
+
+@Mixin(EntityRenderer.class)
+public abstract class EntityRendererMixin {
+	
+	@Inject(method = "func_181560_a", at = @At("RETURN"))
+	private void onRenderReturn (CallbackInfo ci) {
+
+		new RenderEvent().call();
+	}
+}
